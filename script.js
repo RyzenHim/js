7bb82052dbd9721226f5be0211de8d1eeb57ee8f
Himanshu Amulya Upadhyay <90633968+RyzenHim@users.noreply.github.com>
2024-05-20 02:27:52 +0530

    Deloyed

---
+++ script.js
@@ -0,0 +1,47 @@
+// let newbtn = document.createElement("button");
+// newbtn.innerText = "Click Me!";
+// newbtn.style.backgroundColor = "red";
+// newbtn.style.color = "white";
+
+// document.querySelector("body").prepend(newbtn);
+
+// let bgc = document.querySelector("body");
+// bgc.style.backgroundColor = "grey";
+
+// let parajs = document.querySelector(".para");
+// parajs.style.color = "white";
+// parajs.style.fontSize = "30px";
+
+// parajs.innerText = `${parajs.innerText} This is added using JS!`;
+
+// parajs.classList.add("paracss");
+
+// let box = document.querySelector(".square");
+// box.onmouseover = () => {
+//   box.style.backgroundColor = "green";
+// };
+
+//chnage the mode
+// create a button
+
+// let jsbtn = document.createElement("button");
+
+// jsbtn.innerText = "Click Me!";
+
+// let jsbtn = document.querySelector("#btn");
+
+// let bod = document.querySelector("body");
+// let currentMode = "light";
+
+// jsbtn.addEventListener("click", () => {
+//   if (currentMode === "light") {
+//     currentMode = "dark";
+//     // bod.classList.add("dark");
+//     bod.style.backgroundColor = "black";
+//   } else {
+//     currentMode = "light";
+//     // bod.classList.add("light");
+//     document.querySelector("body").style.backgroundColor = "white";
+//   }
+//   console.log(currentMode);
+// });
